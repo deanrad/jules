283e7bec954f6d508e070e5fd351966c94530eb4
Dean Radcliffe <chicagogrooves@gmail.com>
2014-01-26 10:04:31 -0600

    More heroku tweaks

--- web.js
+++ web.js
@@ -3,6 +3,8 @@ var express = require('express');
 var port = process.env.PORT || 5000;
 var app = express();
  
+console.log("Listening on port " + port);
+
 app.get('/', function(request, response) {
     response.sendfile(__dirname + '/app/index.html');
 }).configure(function() {
